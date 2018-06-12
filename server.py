bb9f5c011034b8b7ee04a12529b608ff886ce993
DevinXZhou <devin.x.zhou@gmail.com>
2018-06-12 17:11:27 +0000

    modified to 8 bits
    reversed bits reading order

--- server.py
+++ server.py
@@ -15,30 +15,29 @@ def getint():
     return str(traffic)
 
 
-@app.route('/getbin') #get binary bits sequence of the LED queue, first correspond to LED#2
+@app.route('/getbin') #get binary bits sequence of the LED queue
 def getbin():
     global traffic
     b = bin(traffic)[2:]
-    while (len(b) < 7):
+    while (len(b) < 8):
         b = '0'+b
-    b = b[::-1]
     return b
 
 @app.route('/post') #post an int value to this server and stored as traffic
 def post():
     global traffic
     n = int(request.args.get('n'))
-    if n < 128 and n >= 0:
+    if n < 256 and n >= 0:
         traffic = n
         LED_flags = intToBinArr(traffic)
-        for i in range(0, 7):
-            if i+2 in LED_MAPPING:
-                GPIO.output(LED_MAPPING[i+2], LED_flags[i]) # 0 is on
+        for i in range(1, 8):
+            if i+1 in LED_MAPPING:
+                GPIO.output(LED_MAPPING[i+1], LED_flags[i]) # 0 is on
     else:
-        return "input value is invalid, it should be ranged from 0 to 127"
+        return "input value is invalid, it should be ranged from 0 to 255"
         
     
-    return "request is proceeded, data is modified to " + str(traffic) + "and the bits squence are " + getbin()
+    return "int:" + str(traffic) + ", bits: " + getbin()
 
 @app.route('/alloff')
 def alloff():
@@ -51,7 +50,7 @@ def alloff():
 def allon():
     global traffic
     GPIO.output(DetectorGPIOs, False)
-    traffic = 127
+    traffic = 255
     return "all LED are turned on"
 
 @app.route('/reset')
@@ -60,7 +59,6 @@ def reset():
     GPIO.cleanup()
     configTSP50()
     return "TSP50 is cleaned up, and reseted"
-
 
 ########## Control TSP50 LED ###########
 import RPi.GPIO as GPIO
@@ -72,15 +70,22 @@ GPIO_BCM_channels = [2,3,4,17,27,22,10,9,11,5,6,13,19,26,
 DetectorGPIOs = [2, 3, 4, 17, 27, 22, 10]
 LED_MAPPING = {2:2, 3:3, 4:4, 5:17, 6:27, 7:22, 8:10}
 
+def DETECTOR_ON_ALL():
+        GPIO.output(DetectorGPIOs, False)
+        return 0
+
+def DETECTOR_OFF_ALL():
+        GPIO.output(DetectorGPIOs, True)
+        return 0
 
 def configTSP50():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(GPIO_BCM_channels, GPIO.OUT)
-    alloff()
+    DETECTOR_OFF_ALL()
 
-def intToBinArr(n): #convert an int to bits array
-    b = bin(127-n)[2:] #complement exp. '0b0000110' => '0b1111001', there are total 7 LEDs 7 bits
-    while (len(b) < 7):
+def intToBinArr(n): #convert an int (8 bit int) to bits array
+    b = bin(255-n)[2:] #complement exp. '0b0000110' => '0b1111001', there are total 7 LEDs 7 bits
+    while (len(b) < 8):
         b = '0'+b
     arr = list(map(int, list(b)))
     arr = arr[::-1]
